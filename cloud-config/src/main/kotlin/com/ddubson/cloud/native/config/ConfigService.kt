48e71c75439c5c21fbf221e8e943ef66b00ab97a
Dmitriy Dubson <d.dubson@gmail.com>
2017-10-28 14:52:21 -0400

    Updated BOM to Dalston.SR4; Zipkin tracing now working

--- cloud-config/src/main/kotlin/com/ddubson/cloud/native/config/ConfigService.kt
+++ cloud-config/src/main/kotlin/com/ddubson/cloud/native/config/ConfigService.kt
@@ -2,12 +2,10 @@ package com.ddubson.cloud.native.config
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 class ConfigService
 
 fun main(args: Array<String>) {
