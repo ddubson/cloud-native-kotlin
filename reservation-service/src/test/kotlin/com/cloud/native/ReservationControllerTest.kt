454d084765eb8b7506e58eff9e00e57dfd6a219c
Dmitriy Dubson <d.dubson@gmail.com>
2017-11-05 19:27:31 -0500

    Added reservation-service repository and controller tests

---
+++ reservation-service/src/test/kotlin/com/cloud/native/ReservationControllerTest.kt
@@ -0,0 +1,27 @@
+package com.cloud.native
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@SpringBootTest(classes = arrayOf(ReservationServiceApplication::class))
+@RunWith(SpringRunner::class)
+@AutoConfigureMockMvc
+class ReservationControllerTest {
+    @Autowired
+    lateinit var mvc: MockMvc
+
+    @Test
+    fun `reservation by id should return a jsonified reservation`() {
+        this.mvc.perform(MockMvcRequestBuilders.get("/reservations/1"))
+                .andExpect(status().is2xxSuccessful)
+                .andExpect(content().contentType("application/hal+json;charset=UTF-8"))
+                .andExpect(jsonPath("@.reservationName").value("John"))
+    }
+}
---
+++ reservation-service/src/test/kotlin/com/cloud/native/ReservationRepositoryTest.kt
@@ -0,0 +1,39 @@
+package com.cloud.native
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = arrayOf(ReservationServiceApplication::class))
+@DataJpaTest
+internal class ReservationRepositoryTest {
+    @Autowired
+    lateinit var testEntityManager: TestEntityManager
+
+    @Autowired
+    lateinit var reservationRepository: ReservationRepository
+
+    @Test
+    fun `save should create a new reservation`() {
+        val reservation = Reservation(0, "a reservation")
+        val savedReservation = this.testEntityManager.persistFlushFind(reservation)
+        then(savedReservation.id).isNotNull()
+        then(savedReservation.reservationName).isEqualToIgnoringCase("a reservation")
+    }
+
+    @Test
+    fun `findByName should find reservations by name`() {
+        val reservation = Reservation(0, "rev1")
+        this.testEntityManager.persist(reservation)
+        val foundReservations: List<Reservation> = reservationRepository
+                .findByReservationName("rev1") as List<Reservation>
+        then(foundReservations.size).isEqualTo(1)
+        then(foundReservations[0].reservationName).isEqualTo("rev1")
+    }
+}
