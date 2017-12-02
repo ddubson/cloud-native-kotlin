23571d5f8f369992380ac1a30a5e891a526ede08
Dmitriy Dubson <d.dubson@gmail.com>
2017-12-02 12:37:16 -0500

    Fixed failing producer contract

--- reservation-service/src/test/java/com/cloud/reservation/service/contract/ReservationControllerBase.java
+++ reservation-service/src/test/java/com/cloud/reservation/service/contract/ReservationControllerBase.java
@@ -1,32 +1,25 @@
 package com.cloud.reservation.service.contract;
 
-import com.cloud.reservation.service.Reservation;
-import com.cloud.reservation.service.ReservationRepository;
+import com.cloud.reservation.service.ReservationServiceApplication;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
-
-import java.util.List;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-import static org.mockito.BDDMockito.given;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 @AutoConfigureMessageVerifier
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {ReservationServiceApplication.class})
 public abstract class ReservationControllerBase {
-    @Mock
-    ReservationRepository reservationRepository;
+
+    @Autowired
+    private WebApplicationContext webAppContextSetup;
 
     @Before
     public void setup() {
-        List<Reservation> reservationList = Stream.of("John", "Paul", "Jane", "Mark")
-                .map(name -> new Reservation(0, name)).collect(toList());
-
-        given(reservationRepository.findAll()).willReturn(reservationList);
-        RestAssuredMockMvc.standaloneSetup(reservationRepository);
+        RestAssuredMockMvc.webAppContextSetup(webAppContextSetup);
     }
 }
