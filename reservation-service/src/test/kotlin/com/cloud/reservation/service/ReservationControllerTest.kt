c3463725d08c02816e9e60d7dfa282c1cb2af4e9
Dmitriy Dubson <d.dubson@gmail.com>
2017-12-26 17:22:03 -0500

    Added asciidoc plugin to reservation-service

--- reservation-service/src/test/kotlin/com/cloud/reservation/service/ReservationControllerTest.kt
+++ reservation-service/src/test/kotlin/com/cloud/reservation/service/ReservationControllerTest.kt
@@ -3,8 +3,10 @@ package com.cloud.reservation.service
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -13,6 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 @SpringBootTest(classes = [ReservationServiceApplication::class])
 @RunWith(SpringRunner::class)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir="build/snippets")
 class ReservationControllerTest {
     @Autowired
     lateinit var mvc: MockMvc
@@ -23,5 +26,6 @@ class ReservationControllerTest {
                 .andExpect(status().is2xxSuccessful)
                 .andExpect(content().contentType("application/hal+json;charset=UTF-8"))
                 .andExpect(jsonPath("@.reservationName").value("John"))
+                .andDo(MockMvcRestDocumentation.document("reservation-by-id"))
     }
 }
