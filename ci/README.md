1f2936cdadcadcf28172445c984bb0d358073ade
Dmitriy Dubson <d.dubson@gmail.com>
2017-12-28 13:40:45 -0500

    Added sample Jenkins pipeline via Job DSL groovy script; optimized publishing contract stubs

---
+++ ci/README.md
@@ -0,0 +1,17 @@
+# Continuous Integration
+
+## Jenkins 
+
+## Pipeline Plugin
+
+[Pipeline Plugin](https://jenkins.io/doc/book/pipeline/)
+
+`scripts/pipeline.gdsl` defines the language for executing various functions in a pipeline 
+(using the Pipeline Jenkins plugin)
+
+## Job DSL Plugin
+
+[Job DSL Plugin](https://github.com/jenkinsci/job-dsl-plugin)
+
+`scripts/job_dsl.groovy` defines the Jenkins job layout 
+(using the Job DSL Jenkins plugin)`
--- ci/scripts/job_dsl.groovy
+++ ci/scripts/job_dsl.groovy
@@ -2,8 +2,51 @@ import javaposse.jobdsl.dsl.DslFactory
 
 DslFactory factory = this
 
-factory.job("job-dsl-simple-job") {
-    steps {
-        shell('echo "hello world"')
+factory.job("simple-gradle-boot-build") {
+    deliveryPipelineConfiguration("Build")
+
+    triggers {
+        githubPush()
     }
+
+    scm { github("ddubson/cloud-native-kotlin") }
+
+    wrappers {
+        colorizeOutput()
+    }
+
+    steps {
+        shell("./gradlew clean")
+        shell("./gradlew publishContractStubs")
+        shell("./gradlew build")
+    }
+
+    publishers {
+        archiveJunit("**/build/test-results/test/TEST-*.xml")
+        archiveArtifacts("build/libs/*.jar")
+        downstreamParameterized {
+            trigger("simple-gradle-boot-deploy") {
+                triggerWithNoParameters()
+            }
+        }
+    }
+}
+
+factory.job("simple-gradle-boot-deploy") {
+    deliveryPipelineConfiguration("Deployment")
+    scm { github("ddubson/cloud-native-kotlin") }
+    steps {
+        shell('echo "Deploying Artifact!"')
+    }
+}
+
+factory.deliveryPipelineView("simple-gradle-boot-pipeline-view") {
+    pipelines {
+        component("Deployment", "simple-gradle-boot-build")
+    }
+    allowPipelineStart()
+    showChangeLog()
+    showDescription()
+    showTotalBuildTime()
+    allowRebuild()
 }
